d2936346c37482d42efd4f93e753853ac3441510
Lyssie <lyssiewright.15@gmail.com>
2022-02-07 16:41:59 -0600

    commit tree 8f11648d806359b65efe9d44104f4a73d0fd1729

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(event){
+    event = document.querySelector('#text')
+    event.textContent = "This is really cool!";
+});
+
